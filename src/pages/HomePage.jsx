fdaf854030e1e9a34ee9d5d23f574b396c428993
HomamZituni <homam93@msn.com>
2026-01-08 23:03:37 -0500

    fixed HomePage with categories

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
 import { useFetch } from '../hooks/useFetch';
 import { Link } from 'react-router-dom';
+const categories = ['Seafood', 'Beef', 'Chicken', 'Dessert', 'Vegetarian', 'Pasta', 'Breakfast'];
+
 
 export function HomePage() {
   const { data, loading, error } = useFetch('https://www.themealdb.com/api/json/v1/1/categories.php');
